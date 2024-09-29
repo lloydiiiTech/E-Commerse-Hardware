4ab7021ef334343db14e4b36b6aee0b1c513a1c0
lloydiiiTech <danlloydclanor29@gmail.com>
2024-09-29 12:06:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session'); // Import express-session
+const flash = require('express-flash'); // Import express-flash
+const routes = require('./routes/router.js');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up express-session
+app.use(session({
+    secret: 'your_secret_key', // Replace with your own secret key
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Set up express-flash
+app.use(flash());
+
+// Use routes
+app.use('/', routes);
+
+app.listen(4500, () => {
+    console.log('Server initialized on http://localhost:4500');
+});
